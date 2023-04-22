3f6a7d68f8cce661c4dcdd25c366360a027b70a9
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-22 23:23:39 +0300

    Add Example 006 with array

---
+++ Example006/Program.cs
@@ -0,0 +1,18 @@
+﻿// Имеется одномерный массив array из n элементов, требуется найти элемент массива, равный find, если элемент найден, алгоритм завершает работу
+
+int[] array = {1, 2, 3, 4, 8, 5, 6, 7, 8};
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
